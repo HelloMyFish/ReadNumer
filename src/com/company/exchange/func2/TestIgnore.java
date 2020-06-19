09be87d5bc7dc2b6b74e1b25ee0c1ff9c819842e
gengyufan <gyf@ebidding.net.cn>
2020-06-19 23:12:57 +0800

    添加ignore文件

---
+++ src/com/company/exchange/func2/TestIgnore.java
@@ -0,0 +1,16 @@
+package com.company.exchange.func2;
+
+
+
+/**
+ * @Title:
+ * @Package
+ * @Description:
+ * @Author gyf
+ * @Date 2020/6/19 23:10
+ */
+public class TestIgnore {
+    public static void main(String[] args) {
+        System.out.println(13);
+    }
+}
